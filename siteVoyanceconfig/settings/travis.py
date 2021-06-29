9c99bfc10d1da103baf92d1c73b8f1f4f798b53a
nuno <nuno.ricardo.mars@gmail.com>
2021-06-29 16:54:00 +0200

    add travis service

---
+++ siteVoyanceconfig/settings/travis.py
@@ -0,0 +1,12 @@
+from . import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'purbeurre',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    },
+}
