25eda40c0e1b5d8b3eb92105e0deb59bba1c6e45
NunoMars <nuno.ricardo.mars@gmail.com>
2022-10-06 22:32:19 +0200

    Feat( adding export database multiformats)

--- ball8/admin.py
+++ ball8/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Sentences
-
+from import_export.admin import ImportExportMixin
+class TaskAdmin(ImportExportMixin, admin.ModelAdmin):
+    pass
 # Register your models here.
-admin.site.register(Sentences)
+admin.site.register(Sentences, TaskAdmin)
