c1d8e47568fb80ac5b3ced017c579f78e03ac212
NunoMars <nuno.ricardo.mars@gmail.com>
2022-10-06 22:00:10 +0200

    make repath

--- clairvoyance/urls.py
+++ clairvoyance/urls.py
@@ -6,7 +6,7 @@ from .views import clairvoyance, clairvoyante, user_history, card_deck, card_det
 
 urlpatterns = [
     path("", clairvoyance, name="clairvoyance"),
-    path("clairvoyante", csrf_exempt(clairvoyante), name="clairvoyante"),
+    re_path("clairvoyante", csrf_exempt(clairvoyante), name="clairvoyante"),
     path("history", user_history, name="history"),
     path("card_deck", card_deck, name="card_deck"),
     re_path(r"^card_detail/(?P<card>[0-9]+)/$", card_detail, name="card_detail"),
