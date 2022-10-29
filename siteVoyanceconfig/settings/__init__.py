4d3be79760c10a5a786af071fa9688cfca16dc2d
NunoMars <nuno.ricardo.mars@gmail.com>
2022-10-29 23:18:06 +0200

    false

--- siteVoyanceconfig/settings/__init__.py
+++ siteVoyanceconfig/settings/__init__.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY") or "testkey"
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["herokuapps.com", "localhost", "127.0.0.1"]
 
