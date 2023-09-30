871b70d69e946cb8ca1a50e62833dc3567da059b
NunoMars <nuno.ricardo.mars@gmail.com>
2023-09-30 17:43:49 +0200

    travail views

--- ball8/views.py
+++ ball8/views.py
@@ -5,20 +5,18 @@ from random import choice as ch
 from .models import SentencesFr
 
 
-def ball8(request):
-    args = {}
-    model = SentencesFr
-    if request.method == "GET" and request.GET.get("bouton_submit"):
+def get_random_sentence(model):
+    sentences = model.objects.all()
+    return ch(sentences).sentence
 
+def ball8(request):
+    if request.method == "GET" and request.GET.get("bouton_submit"):
         user_question = request.GET["question"]
         page_title = "ORACLE"
-        args["page_title"] = page_title
-        args["user_question"] = user_question
-
-        sentences = model.objects.all()
-        rand_message = ch(sentences)
-
-        ball8_message = rand_message.sentence
-        args["ball8_message"] = ball8_message
-
-    return render(request, "ball8/ball8.html", args)
+        ball8_message = get_random_sentence(SentencesFr)
+        return render(request, "ball8/ball8.html", {
+            "page_title": page_title,
+            "user_question": user_question,
+            "ball8_message": ball8_message
+        })
+    return render(request, "ball8/ball8.html")
