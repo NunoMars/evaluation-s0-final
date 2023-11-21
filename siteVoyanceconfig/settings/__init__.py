30b9b40e5c112eb613b37e9a2c7899157c849cb6
NunoMars <nuno.ricardo.mars@gmail.com>
2023-11-21 19:35:14 +0100

    add 127

--- siteVoyanceconfig/settings/__init__.py
+++ siteVoyanceconfig/settings/__init__.py
@@ -29,7 +29,7 @@ SECRET_KEY = "\\\ta(xZQ;xUIly1X@IGT:4re$"
 
 DEBUG = "PROD" not in os.environ
 
-ALLOWED_HOSTS = ['nunomars-server.eddi.cloud', 'localhost']
+ALLOWED_HOSTS = ['nunomars-server.eddi.cloud', 'localhost', '127.0.0.1']
 
 
 # Application definition
